fe78e359c9c29a58f47cd7765cad44978926a068
ddm135 | Aut <ddm135@outlook.com>
2024-10-05 21:38:59 +0700

    New tool

--- SSDataReader.pyw
+++ SSDataReader.pyw
@@ -1,22 +1,32 @@
 import tkinter as tk
-from tkinter import filedialog, Toplevel
+from pathlib import Path
+from tkinter import filedialog, messagebox, Toplevel
 from tkinter.ttk import Button, Label
 
-from SSHelpers import ss_write_decrypted, ss_write_encrypted, ss_write_gzipped
+from SSHelpers import (
+    ss_data_write_decrypted,
+    ss_data_write_encrypted,
+    ss_data_write_gzipped,
+)
 from SSTypes import SSData
 
 
-def main():
-    tk_root = tk.Tk()
-    tk_root.withdraw()
-
+def opened(root: tk.Tk):
     path = filedialog.askopenfilename(title="Select data file")
     if not path:
         return
+    path = Path(path)
 
-    data = SSData(path)
+    try:
+        data = SSData(path)
+    except Exception as e:
+        messagebox.showerror(
+            title="Error", message=f"Not a valid SSRG data file: {path}"
+        )
+        return
 
-    win = Toplevel()
+    root.withdraw()
+    win = Toplevel(root)
     win.title("Save As")
     Label(
         win,
@@ -28,25 +38,35 @@ def main():
     Button(
         win,
         text="Close",
-        command=tk_root.destroy,
+        command=root.destroy,
     ).pack(padx=(5, 15), pady=10, side="right")
     Button(
         win,
         text="Decrypted JSON",
-        command=lambda: ss_write_decrypted(data),
+        command=lambda: ss_data_write_decrypted(data),
     ).pack(padx=5, pady=10, side="right")
     Button(
         win,
         text="Encrypted JSON",
-        command=lambda: ss_write_encrypted(data),
+        command=lambda: ss_data_write_encrypted(data),
     ).pack(padx=5, pady=10, side="right")
     Button(
         win,
         text="GZipped Encrypted JSON",
-        command=lambda: ss_write_gzipped(data),
+        command=lambda: ss_data_write_gzipped(data),
     ).pack(padx=(15, 5), pady=10, side="right")
-    win.protocol("WM_DELETE_WINDOW", tk_root.destroy)
-    win.resizable(width=False, height=False)
+    win.protocol("WM_DELETE_WINDOW", root.destroy)
+    win.resizable(False, False)
+
+
+def main():
+    tk_root = tk.Tk()
+    tk_root.title("Open")
+    Button(tk_root, text="Open", command=lambda: opened(tk_root)).pack(
+        padx=15, pady=10, side="top"
+    )
+    tk_root.protocol("WM_DELETE_WINDOW", tk_root.destroy)
+    tk_root.resizable(False, False)
     tk_root.mainloop()
 
 
--- SSHelpers.py
+++ SSHelpers.py
@@ -1,11 +1,22 @@
+from __future__ import annotations
+
 import gzip
+import struct
+from collections.abc import Callable, Generator
 from pathlib import Path
-from tkinter import filedialog
+from tkinter import filedialog, Toplevel
+from tkinter.ttk import Treeview
+from typing import BinaryIO, TYPE_CHECKING
 
-from SSTypes import SSData
+from tksheet import Sheet
+
+from SSConstants import SS_SEQ_LAYOUTS
+
+if TYPE_CHECKING:
+    from SSTypes import SSData, SSSeq
 
 
-def ss_write_gzipped(data: SSData):
+def ss_data_write_gzipped(data: SSData) -> None:
     path = filedialog.asksaveasfilename(
         initialfile=f"{data.path.name.split(".")[0]}.bin",
         filetypes=[("SSRG Packed Data Files", ".bin")],
@@ -21,7 +32,7 @@ def ss_write_gzipped(data: SSData):
         gz.close()
 
 
-def ss_write_encrypted(data: SSData):
+def ss_data_write_encrypted(data: SSData) -> None:
     path = filedialog.asksaveasfilename(
         initialfile=f"{data.path.name.split(".")[0]}",
         filetypes=[("SSRG Unpacked Data Files", "")],
@@ -34,7 +45,7 @@ def ss_write_encrypted(data: SSData):
         f.write(data.encrypted)
 
 
-def ss_write_decrypted(data: SSData):
+def ss_data_write_decrypted(data: SSData) -> None:
     path = filedialog.asksaveasfilename(
         initialfile=f"{data.path.name.split(".")[0]}.json",
         filetypes=[("JSON Files", ".json")],
@@ -46,3 +57,132 @@ def ss_write_decrypted(data: SSData):
 
     with path.open("wb") as f:
         f.write(data.decrypted)
+
+
+def ss_seq_read_int(seq: BinaryIO) -> int:
+    return struct.unpack("<I", seq.read(4))[0]
+
+
+def ss_seq_read_float(seq: BinaryIO) -> float:
+    return struct.unpack("<f", seq.read(4))[0]
+
+
+def ss_seq_read_double(seq: BinaryIO) -> float:
+    return struct.unpack("<d", seq.read(8))[0]
+
+
+def ss_seq_skip_padding(seq: BinaryIO, layout: int) -> None:
+    seq.seek(SS_SEQ_LAYOUTS[layout]["padd_size"], 1)
+
+
+def ss_event_name(code: int):
+    match code:
+        case 0x00:
+            return "Tap"
+        case 0x0B:
+            return "Group 1 Slider Start"
+        case 0x15:
+            return "Group 2 Slider Start"
+        case c if c in range(0x00_00_00_16, 0x7F_FF_FF_FF):
+            return "Group 2 Slider Continue"
+        case _:
+            return "Group 1 Slider Continue"
+
+
+def ss_sequencer_folder_data(
+    sheet: Sheet,
+    root: Toplevel,
+    seqs: Generator[Path, None, None],
+    stop: Callable,
+):
+    from SSTypes import SSSeq
+
+    for seq in seqs:
+        try:
+            seq = SSSeq(seq)
+            sheet.data += [
+                [
+                    seq.path.name,
+                    seq.SEQData_Object[0]["data"].replace("\0", ""),
+                    seq.event_transients["noteCount"],
+                    seq.event_transients["noteCountRaw"],
+                    seq.event_transients["invalidCount"],
+                ]
+            ]
+
+            if stop():
+                break
+        except Exception:
+            pass
+
+    if not stop():
+        sheet.set_all_cell_sizes_to_text()
+        root.minsize(sum(sheet.get_column_widths()) + 20, 291)
+
+
+def ss_sequencer_tempo_data(tree: Treeview, seq: SSSeq, stop: Callable):
+    for idx, tmp in enumerate(seq.SEQData_Tempo):
+        tree.insert(
+            "",
+            "end",
+            values=(
+                idx,
+                tmp["tick"],
+                tmp["tickEnd"],
+                tmp["sec"],
+                tmp["secEnd"],
+                tmp["beatPerMinute"],
+                tmp["beatPerMeasure"],
+                tmp["measurePerBeat"],
+                tmp["measurePerTick"],
+                tmp["tickPerMeasure"],
+                tmp["beatPerSec"],
+                tmp["secPerBeat"],
+                tmp["tickPerSec"],
+                tmp["secPerTick"],
+                tmp["measurePerSec"],
+                tmp["secPerMeasure"],
+                tmp["measureStart"],
+                tmp["measureCount"],
+            ),
+        )
+
+        if stop():
+            break
+
+
+def ss_sequencer_object_data(tree: Treeview, seq: SSSeq, stop: Callable):
+    for idx, obj in enumerate(seq.SEQData_Object):
+        tree.insert(
+            "", "end", values=(idx, obj["property"], obj["dataLen"], obj["data"])
+        )
+
+        if stop():
+            break
+
+
+def ss_sequencer_channel_data(tree: Treeview, seq: SSSeq, stop: Callable):
+    for idx, channel in enumerate(seq.SEQData_Channel):
+        tree.insert("", "end", values=(idx, channel["eventCount"], channel["property"]))
+
+        if stop():
+            break
+
+
+def ss_sequencer_event_data(tree: Treeview, seq: SSSeq, stop: Callable):
+    for event in seq.SEQData_Event:
+        tree.insert(
+            "",
+            "end",
+            values=(
+                event["tick"],
+                event["duration"],
+                event["channelId"],
+                event["objectId"],
+                f"{event["property"]} (0x{event["property"]:X})",
+                event["description"],
+            ),
+        )
+
+        if stop():
+            break
